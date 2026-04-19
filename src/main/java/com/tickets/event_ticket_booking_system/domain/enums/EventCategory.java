00aa7a71a250466a7e44981d0c5a7a3822976354
kidmun <kidusguade41@gmail.com>
2026-04-19 22:20:41 +0300

    add enums

---
+++ src/main/java/com/tickets/event_ticket_booking_system/domain/enums/EventCategory.java
@@ -0,0 +1,4 @@
+package com.tickets.event_ticket_booking_system.domain.enums;
+public enum EventCategory {
+    CONCERT, CINEMA, SPORTS, THEATRE, FLIGHT
+}
---
+++ src/main/java/com/tickets/event_ticket_booking_system/domain/enums/PriceTier.java
@@ -0,0 +1,5 @@
+package com.tickets.event_ticket_booking_system.domain.enums;
+
+public enum PriceTier {
+ STANDARD, PREMIUM, VIP   
+}
