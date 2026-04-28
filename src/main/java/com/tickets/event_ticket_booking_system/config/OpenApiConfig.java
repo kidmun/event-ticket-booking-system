e9cc852441009fdd50a02998b3a8691abd938339
kidmun <kidusguade41@gmail.com>
2026-04-28 13:40:06 +0300

    fix controller issue before payment fix

---
+++ src/main/java/com/tickets/event_ticket_booking_system/config/OpenApiConfig.java
@@ -0,0 +1,30 @@
+package com.tickets.event_ticket_booking_system.config;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Event Ticket Booking System API",
+                version = "1.0",
+                description = "API documentation for Event Ticket Booking System"
+        ),
+        security = {
+                @SecurityRequirement(name = "bearerAuth")
+        }
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        description = "JWT authentication using Bearer token",
+        scheme = "bearer",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        in = SecuritySchemeIn.HEADER
+)
+public class OpenApiConfig {
+}
--- src/main/java/com/tickets/event_ticket_booking_system/controller/PaymentController.java
+++ src/main/java/com/tickets/event_ticket_booking_system/controller/PaymentController.java
@@ -7,6 +7,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -18,7 +19,7 @@ import com.tickets.event_ticket_booking_system.dto.response.PaymentResponse;
 import com.tickets.event_ticket_booking_system.service.PaymentService;
 
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
+
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
